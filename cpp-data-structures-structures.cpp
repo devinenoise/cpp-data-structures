c082cd922f9b55ddd9cd155380646f884db06df4
Kyle Devine <devinenoise@gmail.com>
2020-12-02 12:12:44 -0800

    using structures with bytes and dot ops

--- cpp-data-structures-structures.cpp
+++ cpp-data-structures-structures.cpp
@@ -2,22 +2,23 @@
 
 using namespace std;
 
-struct Rectangle {  // definition
-	int length; //2bytes
-	int width;  //2bytes
-	// total 4 bytes
+struct Rectangle { 
+	int length; 
+	int width;  
+	
 };
 	
-	struct Rectangle r1; // declaration
 
 int main()
 {
 	
-	r1.length = 15; // accesing members with dot operative
-	r1.width = 10; 
-
 	
-	cout << "The area of Rectangle 1 is " << r1.length * r1.width ;
+	struct Rectangle r1 = { 10, 15 };
+
+	r1.length = 20;
+	r1.width = 30;
+	
+	cout << "The area of Rectangle 1 is " << r1.length * r1.width << " and the size of the structure is " << sizeof(r1) << " bytes.";
 
 	return 0;
 
