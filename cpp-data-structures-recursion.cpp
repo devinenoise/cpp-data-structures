df6d4125d42d66c62c30f2821af3d9319d0620d8
Kyle Devine <devinenoise@gmail.com>
2020-12-14 13:18:38 -0800

    using head tail recursion

--- cpp-data-structures-recursion.cpp
+++ cpp-data-structures-recursion.cpp
@@ -6,7 +6,7 @@ void fun(int n) {
 	if (n > 0)					// tail recursion that will print 3 2 1
 	{
 		cout << n << endl;
-		fun(n - 1);
+		fun(n - 1);				// processes at call phase
 	}
 }
 
@@ -16,15 +16,15 @@ void fun2(int n) {
 	if (n > 0)					// head recursion that will print 1 2 3
 	{
 		fun2(n - 1);
-		cout << n << endl;
+		cout << n << endl;		// processes at return phase
 	}
 }
 
-int fun3(int n) {							// using static variable
+int fun3(int n) {							// using a static variable
 	static int x = 0;						
 	if (n > 0) {
 		x++;
-		return fun3(n - 1) + x;				// x is added to n on the return
+		return fun3(n - 1) + x;				// x is added to n on the return phase
 	}
 	return 0;
 }
@@ -39,7 +39,7 @@ int main()
 	
 	
 	int r;
-	r = fun3(5);							// x becomes 5
+	r = fun3(5);							// r is 5 and x increments to the value of r
 	cout << r << endl;						// output 25 (r^2)
 
 
