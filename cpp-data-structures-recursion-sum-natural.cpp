6c98001ad8f765d87c8a5ab2b1d77ff5adda5f75
Kyle Devine <devinenoise@gmail.com>
2020-12-16 12:27:03 -0800

    commented more

--- cpp-data-structures-recursion-sum-natural.cpp
+++ cpp-data-structures-recursion-sum-natural.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int sum(int n) {					// recursive function to find the sum of natural number N
-	if (n == 0)
+int sum(int n) {					// Recursive function to find the sum of natural number N
+	if (n == 0)						// time and space taken is 0(n)
 		return 0;
 	else
 		return sum(n - 1) + n;
 }
 
-int sum2(int n) {					// fastest method using formula to find sum -- 0(1) time taken
-	return n * (n + 1) / 2;			
-}
+int sum2(int n) {					// fastest method using formula to find sum -- 
+	return n * (n + 1) / 2;			// 0(1)	time taken
+	}
 
-int sum3(int n) {					// for loop to find the sum -- 0(n) time taken
-	int i, int s = 0;
+int Isum(int n) {					// Iterative method is for loop to find the sum -- 0(n) time taken
+	int i, s = 0;					// i = iteration count, s = sum
 	for (i = 1; i <= n; i++)
 		s = s + i;
 	return s;
@@ -21,6 +21,10 @@ int sum3(int n) {					// for loop to find the sum -- 0(n) time taken
 
 int main()
 {
+
+	int r;
+	r = Isum(5);
+	cout << r << endl;
 
 	return 0;
 
