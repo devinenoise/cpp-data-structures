576fb8a3fee087f0ab02c91ab9efc94d12eebdc6
Kyle Devine <devinenoise@gmail.com>
2020-12-18 15:17:12 -0800

    tower of hanoi problem

--- cpp-data-structures-towerofhanoi.cpp
+++ cpp-data-structures-towerofhanoi.cpp
@@ -4,16 +4,14 @@ using namespace std;
 // Tower of Hanoi Problem
 // There are three towers (A,B,C) and one of the towers has some numbers of discs of different sizes.  The problem is to transfer all the disks from tower A to tower C, one disk at a time and a larger disk can not sit on top of any smaller disk.
 
-void TOH(int n, int A, int B, int C) {
+void TOH(int n, int A, int B, int C) {              // n is number of disks
 	if (n > 0)
 	{
 		TOH(n - 1, A, C, B);
-			cout << "Disks were moved from " << A << " to "<< C << endl;
-			TOH(n - 1, B, A, C);
+		cout << "A disk was moved from " << A << " to "<< C << endl;
+		TOH(n - 1, B, A, C);
 	}
  }
-
-
 
 
 int main() {
