76bbe8f0143b992acb0a2888613948ae35ee543d
Kyle Devine <devinenoise@gmail.com>
2021-01-05 13:04:03 -0800

    sorting arrays with comments

--- cpp-data-structures-array-sort.cpp
+++ cpp-data-structures-array-sort.cpp
@@ -27,12 +27,12 @@ void InsertSort(struct Array *arr, int x) {				// insert a new element into a so
 	if (arr->length == arr->size)
 		return;
 
-	while (i>=0 && arr->A[i] > x) {							// checks index value is above 0
-		arr->A[i + 1] = arr->A[i];
+	while (i>=0 && arr->A[i] > x) {					// checks index value is above 0 and greater		                                                  than the number being inserted
+		arr->A[i + 1] = arr->A[i];					// shifts the checked number to the right 1 position
 		i--;
 	}
-	arr->A[i + 1] = x;
-	arr->length++;
+	arr->A[i + 1] = x;								// insert new element
+	arr->length++;									// increase array length
 }
 
 
@@ -54,27 +54,25 @@ void swap(int* x, int* y)							// swap function using pointers
 	*y = temp;
 }
 
-void Rearrange(struct Array* arr) {
+void Rearrange(struct Array* arr) {					// sort negatives on the left & positives on the right
 
-	int i = 0;
-	int j = arr->length - 1;
+	int i = 0;										// start at [0]
+	int j = arr->length - 1;						// start at array end
 
 	while(i<j)
 	{
-		while (arr->A[i] < 0) 
+		while (arr->A[i] < 0)						// i increments until it reaches a positive
 			i++;
 		
-		while (arr->A[j] >= 0)
+		while (arr->A[j] >= 0)						// j decrements until it reaches a negative
 		j--;
 		
-		if (i < j)
-		swap(&arr->A[i], &arr->A[j]);
+		if (i < j)									// negatives on left, positives on right
+		swap(&arr->A[i], &arr->A[j]);				// swap negative with positive
 	}
 }
 
 int main() {
-
-
 
 	struct Array arr = { {2,-3,5,10,-15,-7},10,6 };
 
