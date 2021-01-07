03bc6e8a3d8dbc7b1b0b627083221e6259699d36
Kyle Devine <devinenoise@gmail.com>
2021-01-07 15:04:38 -0800

    finding a missing element in an array

---
+++ cpp-data-structures-array-find-single-missing-element.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+struct Array {
+	int A[15];
+	int length;
+	int size;
+};
+
+
+int MissingElement(Array arr) {
+	int diff, i, k, low, high;
+	low = arr.A[0];
+	high = arr.A[9];
+	diff = low - 0;
+
+	for (i = 0; i < arr.length; i++) 
+	{
+		
+		if (arr.A[i] - i != diff)
+		{
+			while (diff < arr.A[i] - i) {
+				cout << "The missing element(s) are " << i + diff << endl;
+				diff++;
+			}
+		}
+	}
+
+return 0;
+}
+
+
+
+
+int main() {
+	
+	Array arr1 = {{1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 15, 17}, 12 };
+	
+	MissingElement(arr1);
+
+	return 0;
+}
